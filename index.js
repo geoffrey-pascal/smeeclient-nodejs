3204ba5b51580023a7f08831ce75572f1a0b95da
Geoffrey Pascal <geoffrey.pascal@fr.ibm.com>
2019-08-01 17:52:19 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const SmeeClient = require('smee-client')
+
+const SMESOURCE=process.env.SMEESOURCE
+const HTTPTARGET=process.env.HTTPTARGET
+
+const smee = new SmeeClient({
+  source: SMEESOURCE,
+  target: HTTPTARGET,
+  logger: console
+})
+
+const events = smee.start()
+
+// Stop forwarding events
+events.close()
